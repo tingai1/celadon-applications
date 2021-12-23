2bfc675108e39c07c231d963ea9206e16e08f286
Ai, Ting <ting.a.ai@intel.com>
2021-12-23 17:27:30 +0800

    Add drag and drop agent
    It communcates with host and Wechat
    
    Tracked-On: OAM-100024
    Signed-off-by: Ai, Ting <ting.a.ai@intel.com>

---
+++ DnDAgent/src/com/intel/dndagent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.intel.dndagent;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState) ;
+        finish();
+    }
+}
+
